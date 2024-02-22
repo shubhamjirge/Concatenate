404acd69f28aade55fd11891aecb72b66c4604ef
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2024-02-22 10:26:12 +0530

    Add files via upload

---
+++ Concatenate.py
@@ -0,0 +1,22 @@
+"""
+Title     : Concatenate
+Subdomain : Numpy
+Domain    : Python
+"""
+
+
+import numpy
+
+n, m, p = map(int, input().split())
+
+ar1 = []
+ar2 = []
+for _ in range(n):
+    tmp = list(map(int, input().split()))
+    ar1.append(tmp)
+for _ in range(m):
+    tmp = list(map(int, input().split()))
+    ar2.append(tmp)
+np_ar1 = numpy.array(ar1)
+np_ar2 = numpy.array(ar2)
+print(numpy.concatenate((np_ar1, np_ar2), axis=0))
